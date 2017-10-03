68cd811626e676e188aa6482c1d28ce081fd483f
hogesuke <miyado@gmail.com>
2017-10-04 02:34:01 +0900

    投稿日時を表示する実験

--- chrome-extension/js/script.js
+++ chrome-extension/js/script.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   chrome.runtime.sendMessage({ command: 'fetchPostedDate' }, datetime => {
     console.debug('datetime:', datetime);
+
+    const dom = document.createElement('div');
+    dom.innerText = datetime;
+    dom.style.backgroundColor = '#000';
+    dom.style.color = '#FFF';
+
+    const target = document.querySelector('.ArticleMainHeader');
+    target.parentNode.insertBefore(dom, target.nextSibling);
   });
 });
