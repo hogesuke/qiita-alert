db905c9c272191533624220feedb23d2cd845ebf
hogesuke <miyado@gmail.com>
2017-10-04 02:24:31 +0900

    編集履歴のpaginationに対応

--- chrome-extension/js/background.js
+++ chrome-extension/js/background.js
@@ -1,30 +1,66 @@
-const onMessage = (request, sender, sendResponse) => {
-  if (request.command === 'fetchPostedDate') {
-    chrome.windows.getCurrent(currentWindow => {
-      chrome.tabs.query({ active: true, windowId: currentWindow.id }, async activeTabs => {
+class Scraper {
 
-        if (!/^https:\/\/qiita\.com\/.+?\/items\/[0-9a-z]{20}$/.test(activeTabs[0].url)) {
-          sendResponse(null);
-          return;
-        }
+  static async scrapePostedDate() {
+    const url = await this._getUrl();
 
-        const url = activeTabs[0].url + '/revisions';
-        const res = await fetch(url);
-        const content = await res.text();
+    if (!this._isMachedUrl(url)) {
+      return null;
+    }
 
-        const revisionsPage = document.createElement('div');
-        revisionsPage.innerHTML = content;
+    let pageDOM = await this._fetchRevisionPageDOM(url, 1);
 
-        const time = revisionsPage.querySelector('.tableList_item:last-child .snapshot_meta time');
+    if (pageDOM.querySelector('.pagination')) {
+      const link = pageDOM.querySelector('.pagination > li:last-child > a');
+      const href = link. getAttribute('href');
+      const m = href.match(/page=([\d]+)$/);
 
-        sendResponse(time.getAttribute('datetime'));
+      if (!m) {
+        return null;
+      }
+
+      pageDOM = await this._fetchRevisionPageDOM(url, m[1]);
+    }
+
+    const time = pageDOM.querySelector('.tableList_item:last-child .snapshot_meta time');
+
+    if (!time || !time.getAttribute('datetime')) {
+      return null;
+    }
+
+    return time.getAttribute('datetime');
+  }
+
+  static async _getUrl() {
+    return new Promise((resolve) => {
+      chrome.windows.getCurrent(currentWindow => {
+        chrome.tabs.query({active: true, windowId: currentWindow.id}, activeTabs => {
+          resolve(activeTabs[0].url);
+        });
       });
     });
+  }
 
+  static _isMachedUrl(url) {
+    return /^https:\/\/qiita\.com\/.+?\/items\/[0-9a-z]{20}$/.test(url);
+  }
+
+  static async _fetchRevisionPageDOM(url, page) {
+    const revisionsUrl = `${url}/revisions?page=${page}`;
+    const res = await fetch(revisionsUrl);
+    const content = await res.text();
+
+    const pageDOM = document.createElement('div');
+    pageDOM.innerHTML = content;
+
+    return pageDOM;
+  }
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.command === 'fetchPostedDate') {
+    Scraper.scrapePostedDate().then(date => sendResponse(date));
     return true;
   } else {
     sendResponse(null);
   }
-};
-
-chrome.runtime.onMessage.addListener(onMessage);
+});
