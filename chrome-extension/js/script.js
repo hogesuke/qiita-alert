04cf6b6f0d54896605dd6110055a630b93cfa441
hogesuke <miyado@gmail.com>
2017-10-08 02:23:31 +0900

    styleを修正, 編集されていない投稿にも対応

--- chrome-extension/js/script.js
+++ chrome-extension/js/script.js
@@ -1,30 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const postDateDom = document.querySelector('meta[itemprop="datePublished"]');
-  const datetime = postDateDom && postDateDom.getAttribute('content');
+  const postDateDom = (
+    document.querySelector('meta[itemprop="datePublished"]') ||
+    document.querySelector('time[itemprop="datePublished"]')
+  );
+  const datetime = (
+    (postDateDom && postDateDom.getAttribute('content')) ||
+    (postDateDom && postDateDom.getAttribute('datetime'))
+  );
 
   if (!datetime) {
     return;
   }
 
+  moment.locale('ja');
+
+  const postedDate = moment(Date.parse(datetime));
+  const noticeLevel = (() => {
+    if (postedDate.isBefore(moment().subtract(2, 'year'))) {
+      return 'danger';
+    }
+    if (postedDate.isBefore(moment().subtract(1, 'year'))) {
+      return 'warning';
+    }
+    return 'safety';
+  })();
+
   const modified = !!document.querySelector('time[itemprop="dateModified"]');
 
-  // 更新していない記事には投稿日時が表示されているので投稿日時の挿入処理をスキップ
   if (modified) {
-    moment.locale('ja');
-
-    const postedDate = new Date(Date.parse(datetime));
     const formattedDate = [
-      moment(postedDate).format('YYYY年MM月DD日'),
+      postedDate.format('YYYY年MM月DD日'),
       'に投稿',
-      ` (${moment(postedDate).fromNow()})`
+      ` (${postedDate.fromNow()})`
     ].join('');
 
     const dom = document.createElement('div');
-    dom.className = 'ArticleAsideHeader__date';
+    dom.className = `ArticleAsideHeader__date qiita-alert ${noticeLevel}`;
     dom.innerText = formattedDate;
 
     const target = document.querySelector('.ArticleAsideHeader__date');
     target.parentNode.insertBefore(dom, target);
+  } else {
+    const dom = document.createElement('span');
+    dom.innerText = ` (${postedDate.fromNow()})`;
+
+    const target = document.querySelector('.ArticleAsideHeader__date span');
+    target.className = `qiita-alert ${noticeLevel}`;
+    target.appendChild(dom);
   }
 });
